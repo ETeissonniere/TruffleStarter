394dd71408835cac827eeb8c634f562116b4effa
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-03-03 14:46:38 +0100

    Fix truffle config for gas report

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,15 @@
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mochaGasSettings = {
+  reporter: 'eth-gas-reporter',
+  reporterOptions: {
+    currency: 'USD',
+    gasPrice: 3,
+  },
+}
+
+const mochaArguments = process.env.GAS_REPORTER ? mochaGasSettings : {}
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,7 +22,7 @@ module.exports = {
     },
     ropsten: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY);
+        return new HDWalletProvider(process.env.MNENOMIC, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: '3', // eslint-disable-line camelcase
       gas: 4465030,
@@ -20,7 +30,7 @@ module.exports = {
     },
     kovan: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY);
+        return new HDWalletProvider(process.env.MNENOMIC, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: '42', // eslint-disable-line camelcase
       gas: 4465030,
@@ -28,20 +38,28 @@ module.exports = {
     },
     rinkeby: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY);
+        return new HDWalletProvider(process.env.MNENOMIC, 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: 4, // eslint-disable-line camelcase
       gas: 3000000,
       gasPrice: 10000000000,
     },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xffffffffff,
+      gasPrice: 0x01,
+    },
     // main ethereum network(mainnet)
     main: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY);
+        return new HDWalletProvider(process.env.MNENOMIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: 1, // eslint-disable-line camelcase
       gas: 3000000,
       gasPrice: 10000000000,
     },
   },
-};
+  mocha: mochaArguments,
+}
