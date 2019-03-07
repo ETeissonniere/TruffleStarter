bf99a77f1b6a450c492320af1b5ed9477ff4b7fa
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-03-07 09:13:55 +0100

    Fix typo

--- truffle-config.js
+++ truffle-config.js
@@ -20,9 +20,17 @@ module.exports = {
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY)
+      },
+      network_id: '5', // eslint-disable-line camelcase
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
     ropsten: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: '3', // eslint-disable-line camelcase
       gas: 4465030,
@@ -30,7 +38,7 @@ module.exports = {
     },
     kovan: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: '42', // eslint-disable-line camelcase
       gas: 4465030,
@@ -38,7 +46,7 @@ module.exports = {
     },
     rinkeby: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: 4, // eslint-disable-line camelcase
       gas: 3000000,
@@ -54,7 +62,7 @@ module.exports = {
     // main ethereum network(mainnet)
     main: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNENOMIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY)
       },
       network_id: 1, // eslint-disable-line camelcase
       gas: 3000000,
